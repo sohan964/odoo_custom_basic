2e2963e9f72c2b1c2669031cc27b0942f11789ad
sohan964 <sohanurr791@gmail.com>
2026-02-22 09:35:16 +0600

    learn sequence, noupdate, _rec_name, create method

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -4,15 +4,18 @@ from odoo import models, fields, api
 class HospitalAppointment(models.Model):
     _name="hospital.appointment"
     _description = "Appointment Master"
-    _rec_name = "patient_id" #this for show the name in the appointment
+    _rec_name = "patient_id" #this for show the name in the appointment route
 
     reference = fields.Char(string="Reference", default="New")
     patient_id = fields.Many2one('hospital.patient', string="patient") # make link to the patient table
     date_appointment = fields.Date(string="Appointment Date")
     note = fields.Text(string="Note")
     #23.10
-    #inheriting the create method
-    @api.model_create_multi
+    #inheriting the create method to connect the reference field with sequence
+    @api.model_create_multi 
     def create(self, vals_list):
         print("odoo", vals_list)
+        for vals in vals_list:
+            if not vals.get('reference') or vals['reference']=='New':
+                vals['reference']=self.env['ir.sequence'].next_by_code('hospital.appointment')
         return super().create(vals_list)
